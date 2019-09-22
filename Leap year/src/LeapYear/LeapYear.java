d9437cf977450220ff7c27b035e78a5970fceaf0
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-09-22 15:42:34 +0530

    LeapYear parametrized method to check year is LeapYear or not.

--- Leap year/src/LeapYear/LeapYear.java
+++ Leap year/src/LeapYear/LeapYear.java
@@ -1,8 +1,25 @@
 package LeapYear;
 
+import java.time.Year;
+import java.util.Scanner;
+
 /**
  * @ Author : Rakesh Yadav
  * @ @ Version : 8.0
  */
 public class LeapYear {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+    }
+
+    public int LeapYear(int year){
+        if (year%4 == 0 && year%100 != 0){
+            System.out.println("Leap");
+        }
+        else if (year%400==0 && year%4==0){
+            System.out.println("Leap");
+        }else
+            System.out.println("Regular");
+    }
 }
