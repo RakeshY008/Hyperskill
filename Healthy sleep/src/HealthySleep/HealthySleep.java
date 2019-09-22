4e05e14dcd14234f7c641d3130013a6c74585af2
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-09-22 14:49:55 +0530

    imported Scanner class to  take input by user

--- Healthy sleep/src/HealthySleep/HealthySleep.java
+++ Healthy sleep/src/HealthySleep/HealthySleep.java
@@ -1,4 +1,12 @@
+/**
+ * @ Author : Rakesh Yadav
+ */
 package HealthySleep;
 
+import java.util.Scanner;
 public class HealthySleep {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+    }
 }
