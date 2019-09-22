eccf9d08e9e19faea68ec007759a8404eed6742d
RakeshY008 <rakesh.yadav_cs18@gla.ac.in>
2019-09-22 16:18:22 +0530

    LeapYear Problem final -Commit

--- Leap year/src/LeapYear/LeapYear.java
+++ Leap year/src/LeapYear/LeapYear.java
@@ -1,25 +1,21 @@
 package LeapYear;
-
 import java.time.Year;
 import java.util.Scanner;
-
 /**
  * @ Author : Rakesh Yadav
  * @ @ Version : 8.0
  */
+
 public class LeapYear {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        Year check = Year.of(year);
 
-    }
-
-    public int LeapYear(int year){
-        if (year%4 == 0 && year%100 != 0){
+        if (check.isLeap()){
             System.out.println("Leap");
         }
-        else if (year%400==0 && year%4==0){
-            System.out.println("Leap");
-        }else
+        else
             System.out.println("Regular");
     }
 }
